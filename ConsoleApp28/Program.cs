d3cb6e64d679344b3b1636d12b4c406f849c90f6
lenya <hellchildgg@mail.ru>
2022-12-23 14:29:36 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp28/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double s = 0;
+            Console.WriteLine("Введите значение y:");
+            double y =
+            Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение x:");
+            double x =
+            Convert.ToDouble(Console.ReadLine());
+            s = 9.756 * Math.Pow(y, 7) + 2 *
+            Math.Tan(x);
+            Console.Write("s = " + "{0:0.00}", s);
+            Console.ReadKey();
+        }
+    }
+}
